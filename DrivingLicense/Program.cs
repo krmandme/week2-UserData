04618d604f88d20d8f4eb8c39dff28d95b26d930
Kristjan Mändmets <kristjan.mandmets@ppe.ee>
2022-02-06 16:26:38 +0200

    Control Flow

---
+++ DrivingLicense/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DrivingLicense
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//programm küsib kasutaja vanust
+			//programm arvutab kasutaja vanust
+			//kui kasutaja on noorem, kui 18, siis programm kuvab kosnoolis 
+			// "oled liiga noor, et juhilube saada";
+			//kui kasutaja on vanem, kui 18, siis programm kuvab konsoolis
+			//oled piisavalt vana, et juhilube taodelda!";
+			//kui kasutaja on 18, siis pogramm kuvab konsoolis
+			//"palju õnne! Nüüd saad sa juhiluba taodelda"
+
+			Console.WriteLine("Palun sisesta enda vanus:");
+			int UserAge = Convert.ToInt32(Console.ReadLine());
+
+			if (UserAge < 18)
+			{
+				Console.WriteLine("Oled liiga noor, et juhilube saada!");
+			}
+			else if (UserAge > 18)
+			{
+				Console.WriteLine("Oled piisavalt vana, et juhilube taodelda!");
+			}
+
+			else
+			{
+				Console.WriteLine("Palju õnne!" +
+					" Nüüd saad sa juhilube taodelda!");
+			}
+
+			Console.WriteLine("Kena päeva Teile!");
+
+		}
+	}
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserAge
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//programm küsib kasutaja vanust;
+			//programm arvutab kasutaja sünniaastat;
+			//programm kuvab sünniaastat konsoolis
+			// "oled sündinud aastal {yearOfBirth}"
+
+			Console.WriteLine("Kui vana te olete?");
+			int UserAge = Convert.ToInt32(Console.ReadLine());
+			int yearOfBirth = 2022 - UserAge;
+			Console.WriteLine($"Oled sündinud aastal {yearOfBirth}!");
+
+
+
+		}
+	}
+}
